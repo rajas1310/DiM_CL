7fcbfaafc6f89b933799e16aef0c15105d806e62
rajas1310 <rajaschitale@gmail.com>
2023-08-27 12:13:55 +0000

    Change hyperparams

--- pool_match.py
+++ pool_match.py
@@ -521,22 +521,22 @@ if __name__ == '__main__':
     parser.add_argument('--ipc', type=int, default=50)
     parser.add_argument('--batch-size', type=int, default=64)
     parser.add_argument('--epochs', type=int, default=100)
-    parser.add_argument('--epochs-eval', type=int, default=1000)
+    parser.add_argument('--epochs-eval', type=int, default=100) #100
     parser.add_argument('--epochs-match', type=int, default=100)
     parser.add_argument('--epochs-match-train', type=int, default=16)
     parser.add_argument('--lr', type=float, default=5e-6)
-    parser.add_argument('--eval-lr', type=float, default=0.01)
+    parser.add_argument('--eval-lr', type=float, default=3e-4) #0.01
     parser.add_argument('--momentum', type=float, default=0.9)
     parser.add_argument('--weight-decay', type=float, default=5e-4)
     parser.add_argument('--match-coeff', type=float, default=0.001)
     parser.add_argument('--match-model', type=str, default='convnet')
     parser.add_argument('--match', type=str, default='grad')
-    parser.add_argument('--eval-model', type=str, nargs='+', default=['convnet'])
+    parser.add_argument('--eval-model', type=str, nargs='+', default=['convnet', 'resnet10', 'resnet18'])
     parser.add_argument('--dim-noise', type=int, default=100)
     parser.add_argument('--num-workers', type=int, default=4)
     parser.add_argument('--print-freq', type=int, default=50)
     parser.add_argument('--eval-interval', type=int, default=10)
-    parser.add_argument('--test-interval', type=int, default=200)
+    parser.add_argument('--test-interval', type=int, default=20) #200
     parser.add_argument('--fix-disc', action='store_true', default=False)
 
     parser.add_argument('--data', type=str, default='cifar10')
